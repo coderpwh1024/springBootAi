9e261db9a92eea3725656b99c75ec732b58e6116
coderpwh <pwhfbc@outlook.com>
2024-06-08 15:56:55 +0800

    Add files via upload

---
+++ src/test/java/com/coderpwh/SpringBootAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderpwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
